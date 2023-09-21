ad79d449e60a26f76d0b060f203976dded92d1c2
nathanieladiah <nathanieladiah@gmail.com>
2023-09-21 03:09:43 -0400

    Explore Page

--- src/pages/Explore.jsx
+++ src/pages/Explore.jsx
@@ -1,5 +1,41 @@
+import rentCategoryImage from "../assets/jpg/rentCategoryImage.jpg";
+import sellCategoryImage from "../assets/jpg/sellCategoryImage.jpg";
+
+import { Link } from "react-router-dom";
+
 const Explore = () => {
-  return <div>Explore</div>;
+  return (
+    <div className="explore">
+      <header>
+        <p className="pageHeader">Explore</p>
+      </header>
+      <main>
+        {/* slider */}
+        <p className="exploreCategoryHeading">Categories</p>
+        <div className="exploreCategories">
+          <Link to="/category/rent">
+            <img
+              src={rentCategoryImage}
+              alt="rent"
+              className="exploreCategoryImg"
+            />
+
+            <p className="exploreCategoryName">Places for rent</p>
+          </Link>
+
+          <Link to="/category/sell">
+            <img
+              src={sellCategoryImage}
+              alt="rent"
+              className="exploreCategoryImg"
+            />
+
+            <p className="exploreCategoryName">Places for sale</p>
+          </Link>
+        </div>
+      </main>
+    </div>
+  );
 };
 
 export default Explore;
