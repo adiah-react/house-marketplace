4ea5b05a2bdc016beb3c99af9388066289a4dc72
nathanieladiah <nathanieladiah@gmail.com>
2023-09-21 17:56:21 -0400

    Fetch single listing

---
+++ src/pages/Listing.jsx
@@ -0,0 +1,32 @@
+// import shareIcon from '../assets/svg/shareIcon.svg'
+
+import { doc, getDoc } from "firebase/firestore";
+import { useEffect, useState } from "react";
+import { useNavigate, useParams } from "react-router-dom";
+import { db } from "../firebase.config";
+
+const Listing = () => {
+  const [listing, setListing] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const [shareLinkCopied, setShareLinkCopied] = useState(false);
+
+  const navigate = useNavigate();
+  const params = useParams();
+
+  useEffect(() => {
+    const fetchListing = async () => {
+      const docRef = doc(db, "listings", params.listingId);
+      const docSnap = await getDoc(docRef);
+
+      if (docSnap.exists()) {
+        console.log(docSnap.data());
+        setListing(docSnap.data());
+        setLoading(false);
+      }
+    };
+
+    fetchListing();
+  }, [navigate, params.listingId]);
+  return <div>Listing</div>;
+};
+export default Listing;
